446fb656f621a534c027e4b5a9b9f555bcda7527
blahaj <gwheatley@ftstudent.org>
2024-08-28 12:58:06 -0400

    Add Everything

---
+++ scripts/gamemanager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var score = 0
+
+@onready var score_label = $ScoreLabel
+
+
+func add_point():
+	score += 1
+	if score == 1:
+		score_label.text = "You have collected " +str(score) + " coin."
+	else:
+		score_label.text = "You have collected " + str(score) + " coins."
---
+++ scripts/killzone.gd
@@ -0,0 +1,14 @@
+extends Area2D
+@onready var timer = $Timer
+
+
+func _on_body_entered(body):
+   print("You died.")
+   Engine.time_scale = 0.5
+   timer.start()
+   body.get_node("CollisionShape2D").queue_free()
+
+
+func _on_timer_timeout():
+    Engine.time_scale = 1.0
+    get_tree().reload_current_scene()
